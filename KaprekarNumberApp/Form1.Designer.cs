20a9366fb8f01904b70a93c920be2b7c8e0d4359
Zachary Hall <zacplusplus@gmail.com>
2017-02-27 09:56:08 -0600

    Fixed typos. Final release.

--- KaprekarNumberApp/Form1.Designer.cs
+++ KaprekarNumberApp/Form1.Designer.cs
@@ -45,9 +45,10 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(1120, 663);
+            this.btnSubmit.Location = new System.Drawing.Point(420, 278);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(180, 62);
+            this.btnSubmit.Size = new System.Drawing.Size(68, 26);
             this.btnSubmit.TabIndex = 0;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
@@ -56,35 +57,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1007, 593);
+            this.label1.Location = new System.Drawing.Point(378, 249);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 32);
+            this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "The answer is: ";
             // 
             // lblAnswer
             // 
             this.lblAnswer.AutoSize = true;
-            this.lblAnswer.Location = new System.Drawing.Point(1213, 592);
+            this.lblAnswer.Location = new System.Drawing.Point(455, 248);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(0, 32);
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
             this.lblAnswer.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1238, 593);
+            this.label2.Location = new System.Drawing.Point(464, 249);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 32);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Routines";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1008, 519);
+            this.label3.Location = new System.Drawing.Point(378, 218);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(204, 32);
+            this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Enter a valid #:";
             // 
@@ -95,9 +100,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label4.Location = new System.Drawing.Point(555, 13);
+            this.label4.Location = new System.Drawing.Point(208, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(672, 82);
+            this.label4.Size = new System.Drawing.Size(274, 34);
             this.label4.TabIndex = 6;
             this.label4.Text = "Kaprekar\'s Routine App";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -105,15 +111,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(967, 208);
+            this.label5.Location = new System.Drawing.Point(363, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(593, 224);
+            this.label5.Size = new System.Drawing.Size(224, 91);
             this.label5.TabIndex = 7;
             this.label5.Text = resources.GetString("label5.Text");
             // 
             // numberInput
             // 
-            this.numberInput.Location = new System.Drawing.Point(1244, 513);
+            this.numberInput.Location = new System.Drawing.Point(466, 215);
+            this.numberInput.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.numberInput.Maximum = new decimal(new int[] {
             9999,
             0,
@@ -125,7 +133,7 @@
             0,
             0});
             this.numberInput.Name = "numberInput";
-            this.numberInput.Size = new System.Drawing.Size(130, 38);
+            this.numberInput.Size = new System.Drawing.Size(49, 20);
             this.numberInput.TabIndex = 8;
             this.numberInput.Value = new decimal(new int[] {
             1,
@@ -137,20 +145,22 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(897, 159);
+            this.label6.Location = new System.Drawing.Point(336, 67);
+            this.label6.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(695, 32);
+            this.label6.Size = new System.Drawing.Size(295, 13);
             this.label6.TabIndex = 9;
-            this.label6.Text = "Here are the rues surrounding Kaprekar\'s Routine:";
+            this.label6.Text = "Here are the rules surrounding Kaprekar\'s Routine:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label7.Location = new System.Drawing.Point(92, 208);
+            this.label7.Location = new System.Drawing.Point(34, 87);
+            this.label7.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(756, 450);
+            this.label7.Size = new System.Drawing.Size(286, 184);
             this.label7.TabIndex = 10;
             this.label7.Text = resources.GetString("label7.Text");
             // 
@@ -158,19 +168,20 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(84, 159);
+            this.label8.Location = new System.Drawing.Point(32, 67);
+            this.label8.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(551, 46);
+            this.label8.Size = new System.Drawing.Size(237, 20);
             this.label8.TabIndex = 11;
             this.label8.Text = "What is Kaprekar\'s Routine?";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1781, 821);
+            this.ClientSize = new System.Drawing.Size(674, 365);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -182,8 +193,9 @@
             this.Controls.Add(this.lblAnswer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSubmit);
-            this.MaximumSize = new System.Drawing.Size(1814, 910);
-            this.MinimumSize = new System.Drawing.Size(1813, 909);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.MaximumSize = new System.Drawing.Size(690, 404);
+            this.MinimumSize = new System.Drawing.Size(690, 404);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kaprekar\'s Routine";
